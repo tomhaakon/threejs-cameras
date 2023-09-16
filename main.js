1b4fd56810d7c2ee8c0f83c3d9a96da58576bfac
Tom-Håkon Fjeldheim <tom.fjeldheim@gmail.com>
2023-09-16 21:55:10 +0200

    third

--- main.js
+++ main.js
@@ -9,137 +9,53 @@ import { sendStatus } from './handleStatus.js'
 sendError('loaded', 'main.js') // send msg that main.js is loaded
 
 function main() {
-  //
   const canvas = document.querySelector('#c')
-  const view1Elem = document.querySelector('#view1')
-  const view2Elem = document.querySelector('#view2')
-  const renderer = new THREE.WebGLRenderer({
-    antialias: true,
-    canvas,
-    logarithmicDepthBuffer: true,
-  })
+  const renderer = new THREE.WebGLRenderer({ antialias: true, canvas })
 
-  const size = 1
-  const near = 5
-  const far = 50
+  const left = 0
+  const right = 300 // default canvas size
+  const top = 0
+  const bottom = 150 // defautl canvas size
+  const near = -1
+  const far = 1
   const camera = new THREE.OrthographicCamera(
-    -size,
-    size,
-    size,
-    -size,
+    left,
+    right,
+    top,
+    bottom,
     near,
     far
   )
-  camera.zoom = 0.2
-  camera.position.set(0, 10, 20)
-
-  const cameraHelper = new THREE.CameraHelper(camera)
-
-  class MinMaxGUIHelper {
-    constructor(obj, minProp, maxProp, minDif) {
-      this.obj = obj
-      this.minProp = minProp
-      this.maxProp = maxProp
-      this.minDif = minDif
-    }
-    get min() {
-      return this.obj[this.minProp]
-    }
-    set min(v) {
-      this.obj[this.minProp] = v
-      this.obj[this.maxProp] = Math.max(this.obj[this.maxProp], v + this.minDif)
-    }
-    get max() {
-      return this.obj[this.maxProp]
-    }
-    set max(v) {
-      this.obj[this.maxProp] = v
-      this.min = this.min // this will call the min setter
-    }
-  }
-
-  const gui = new GUI()
-  gui.add(camera, 'zoom', 0.01, 1, 0.01).listen()
-  const minMaxGUIHelper = new MinMaxGUIHelper(camera, 'near', 'far', 0.1)
-  gui.add(minMaxGUIHelper, 'min', 0.1, 50, 0.1).name('near')
-  gui.add(minMaxGUIHelper, 'max', 0.1, 50, 0.1).name('far')
-
-  const controls = new OrbitControls(camera, view1Elem)
-  controls.target.set(0, 5, 0)
-  controls.update()
-
-  const camera2 = new THREE.PerspectiveCamera(
-    60, // fov
-    2, // aspect
-    0.1, // near
-    500 // far
-  )
-  camera2.position.set(16, 28, 40)
-  camera2.lookAt(0, 5, 0)
-
-  const controls2 = new OrbitControls(camera2, view2Elem)
-  controls2.target.set(0, 5, 0)
-  controls2.update()
+  camera.zoom = 1
 
   const scene = new THREE.Scene()
   scene.background = new THREE.Color('black')
-  scene.add(cameraHelper)
 
-  {
-    const planeSize = 40
-
-    const loader = new THREE.TextureLoader()
-    const texture = loader.load('resources/images/checker.png')
-    texture.wrapS = THREE.RepeatWrapping
-    texture.wrapT = THREE.RepeatWrapping
+  const loader = new THREE.TextureLoader()
+  const textures = [
+    loader.load('resources/images/flower-1.jpg'),
+    loader.load('resources/images/flower-2.jpg'),
+    loader.load('resources/images/flower-3.jpg'),
+    loader.load('resources/images/flower-4.jpg'),
+    loader.load('resources/images/flower-5.jpg'),
+    loader.load('resources/images/flower-6.jpg'),
+  ]
+  const planeSize = 256
+  const planeGeo = new THREE.PlaneGeometry(planeSize, planeSize)
+  const planes = textures.map((texture) => {
+    const planePivot = new THREE.Object3D()
+    scene.add(planePivot)
     texture.magFilter = THREE.NearestFilter
-    texture.colorSpace = THREE.SRGBColorSpace
-    const repeats = planeSize / 2
-    texture.repeat.set(repeats, repeats)
-
-    const planeGeo = new THREE.PlaneGeometry(planeSize, planeSize)
-    const planeMat = new THREE.MeshPhongMaterial({
+    const planeMat = new THREE.MeshBasicMaterial({
       map: texture,
       side: THREE.DoubleSide,
     })
     const mesh = new THREE.Mesh(planeGeo, planeMat)
-    mesh.rotation.x = Math.PI * -0.5
-    scene.add(mesh)
-  }
-
-  {
-    const cubeSize = 4
-    const cubeGeo = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize)
-    const cubeMat = new THREE.MeshPhongMaterial({ color: '#8AC' })
-    const mesh = new THREE.Mesh(cubeGeo, cubeMat)
-    mesh.position.set(cubeSize + 1, cubeSize / 2, 0)
-    scene.add(mesh)
-  }
-
-  {
-    const sphereRadius = 3
-    const sphereWidthDivisions = 32
-    const sphereHeightDivisions = 16
-    const sphereGeo = new THREE.SphereGeometry(
-      sphereRadius,
-      sphereWidthDivisions,
-      sphereHeightDivisions
-    )
-    const sphereMat = new THREE.MeshPhongMaterial({ color: '#CA8' })
-    const mesh = new THREE.Mesh(sphereGeo, sphereMat)
-    mesh.position.set(-sphereRadius - 1, sphereRadius + 2, 0)
-    scene.add(mesh)
-  }
-
-  {
-    const color = 0xffffff
-    const intensity = 3
-    const light = new THREE.DirectionalLight(color, intensity)
-    light.position.set(0, 10, 0)
-    light.target.position.set(-5, 0, 0)
-    scene.add(light)
-    scene.add(light.target)
-  }
+    planePivot.add(mesh)
+    // move plane so top left corner is origin
+    mesh.position.set(planeSize / 2, planeSize / 2, 0)
+    return planePivot
+  })
 
   function resizeRendererToDisplaySize(renderer) {
     const canvas = renderer.domElement
@@ -152,70 +68,46 @@ function main() {
 
     return needResize
   }
-  function setScissorForElement(elem) {
-    const canvasRect = canvas.getBoundingClientRect()
-    const elemRect = elem.getBoundingClientRect()
 
-    // compute a canvas relative rectangle
-    const right = Math.min(elemRect.right, canvasRect.right) - canvasRect.left
-    const left = Math.max(0, elemRect.left - canvasRect.left)
-    const bottom = Math.min(elemRect.bottom, canvasRect.bottom) - canvasRect.top
-    const top = Math.max(0, elemRect.top - canvasRect.top)
+  function render(time) {
+    time *= 0.001 // convert to seconds;
 
-    const width = Math.min(canvasRect.width, right - left)
-    const height = Math.min(canvasRect.height, bottom - top)
-
-    // setup the scissor to only render to that part of the canvas
-    const positiveYUpBottom = canvasRect.height - bottom
-    renderer.setScissor(left, positiveYUpBottom, width, height)
-    renderer.setViewport(left, positiveYUpBottom, width, height)
-
-    // return the aspect
-    return width / height
-  }
-  function render() {
-    resizeRendererToDisplaySize(renderer)
-
-    // turn on the scissor
-    renderer.setScissorTest(true)
-
-    // render the original view
-    {
-      const aspect = setScissorForElement(view1Elem)
-
-      // update the camera for this aspect
-      camera.left = -aspect
-      camera.right = aspect
+    if (resizeRendererToDisplaySize(renderer)) {
+      camera.right = canvas.width
+      camera.bottom = canvas.height
       camera.updateProjectionMatrix()
-      cameraHelper.update()
-
-      // don't draw the camera helper in the original view
-      cameraHelper.visible = false
-
-      scene.background.set(0x000000)
-      renderer.render(scene, camera)
     }
 
-    // render from the 2nd camera
-    {
-      const aspect = setScissorForElement(view2Elem)
+    const distAcross = Math.max(20, canvas.width - planeSize)
+    const distDown = Math.max(20, canvas.height - planeSize)
 
-      // update the camera for this aspect
-      camera2.aspect = aspect
-      camera2.updateProjectionMatrix()
+    // total distance to move across and back
+    const xRange = distAcross * 2
+    const yRange = distDown * 2
+    const speed = 180
 
-      // draw the camera helper in the 2nd view
-      cameraHelper.visible = true
+    planes.forEach((plane, ndx) => {
+      // compute a unique time for each plane
+      const t = time * speed + ndx * 300
 
-      scene.background.set(0x000040)
-      renderer.render(scene, camera2)
-    }
+      // get a value between 0 and range
+      const xt = t % xRange
+      const yt = t % yRange
+
+      // set our position going forward if 0 to half of range
+      // and backward if half of range to range
+      const x = xt < distAcross ? xt : xRange - xt
+      const y = yt < distDown ? yt : yRange - yt
+
+      plane.position.set(x, y, 0)
+    })
+
+    renderer.render(scene, camera)
 
     requestAnimationFrame(render)
   }
 
   requestAnimationFrame(render)
-  sendStatus(true)
 }
 
 main()
